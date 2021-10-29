12b6939e834180a5cd8370ac6a646e530bec718b
Roberto Carrillo <roberto.carrillo@michelada.io>
2021-10-29 11:09:56 -0500

    feat: add csv database methods

---
+++ app/lib/csv_database.rb
@@ -0,0 +1,76 @@
+require 'csv'
+
+module CsvDatabase
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def create(args)
+      obj = new(args)
+      CSV.open(csv_file_path, "a") do |csv|
+        csv << obj.to_a
+      end
+      obj
+    end
+
+    def all
+      CSV.read(csv_file_path)
+    end
+  
+    def find(id)
+      all.select { |row| row.first == id }.first
+    end
+  
+    def where(column, value)
+      raise ArgumentError.new 'Unknown column' unless @@csv_attr.include?(column)
+      all.select { |row| row[column] == value }
+    end
+  
+    def delete_all
+      File.delete(csv_file_path) if File.exist?(csv_file_path)
+      set_if_not_exists
+    end
+
+    def attr
+      @@csv_attr
+    end
+
+    private
+  
+    def csv_attr(*attr)
+      attr.map!(&:to_sym)
+      attr_accessor(*attr)
+      @@csv_attr = attr.freeze
+    end
+  
+    def csv_file_path
+      Rails.root.join('storage', "#{self.name.underscore}.csv").to_s
+    end
+  
+    def set_if_not_exists
+      File.open(csv_file_path, 'w') unless File.exist?(csv_file_path)
+    end
+  end
+
+
+
+  def initialize(args = {})
+    raise 'ArgumentError' unless args.is_a?(Hash)
+    set_if_not_exists
+    set_attr(args.transform_keys(&:to_sym))
+  end
+
+  def to_a
+    self.class.attr.map { |attr| instance_variable_get("@#{attr}") }
+  end
+
+  private
+
+  def set_attr(args)
+    args.slice(*self.class.attr).each do |attr, val|
+      instance_variable_set("@#{attr}", val)
+    end
+  end
+end
