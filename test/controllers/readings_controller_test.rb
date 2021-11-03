6091d05115156a564af611cd4d8a7d35700edf1f
Roberto Carrillo <roberto.carrillo@michelada.io>
2021-11-02 19:59:02 -0600

    test: add exception test

--- test/controllers/readings_controller_test.rb
+++ test/controllers/readings_controller_test.rb
@@ -1,6 +1,19 @@
 require "test_helper"
 
 class ReadingsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    File.chmod(0755, Rails.root.join('storage'))
+    Reading.delete_all
+  end
+
+  test 'create with invalid params' do
+    post readings_url, params: {
+      id: "36d5658a-6908-479e-887e-a949ec199272",
+      readings: []
+    }
+    assert_response :unprocessable_entity
+  end
+
   test 'create with valid params' do
     post readings_url, params: {
       id: "36d5658a-6908-479e-887e-a949ec199272",
@@ -18,21 +31,38 @@ class ReadingsControllerTest < ActionDispatch::IntegrationTest
     assert_response :created
   end
 
-  test 'create with invalid params' do
-    post readings_url, params: {
-      id: "36d5658a-6908-479e-887e-a949ec199272",
-      readings: []
-    }
-    assert_response :unprocessable_entity
-  end
-
   test 'show' do
+    Reading.create(
+      id: "36d5658a-6908-479e-887e-a949ec199272",
+      timestamp: "2021-09-29T16:08:15+01:00",
+      count: 2
+    )
     get reading_url(id: "36d5658a-6908-479e-887e-a949ec199272")
     assert_response :success
   end
 
   test 'show count' do
+    Reading.create(
+      id: "36d5658a-6908-479e-887e-a949ec199272",
+      timestamp: "2021-09-29T16:08:15+01:00",
+      count: 2
+    )
     get count_reading_url(id: "36d5658a-6908-479e-887e-a949ec199272")
     assert_response :success
+  end
+
+  test 'file a permission issue' do
+    File.chmod(0000, Rails.root.join('storage'))
+    assert_raise(Errno::EACCES) do
+      Reading.create(
+        id: "36d5658a-6908-479e-887e-a949ec199272",
+        timestamp: "2021-09-29T16:08:15+01:00",
+        count: 2
+      )
+    end
+  end
+
+  def teardown
+    File.chmod(0755, Rails.root.join('storage'))
   end
 end
