c979426a5901986dcd5c7ccb8195d96591d14162
Roberto Carrillo <roberto.carrillo@michelada.io>
2021-11-04 15:24:30 -0600

    adjust file permissions test

--- test/controllers/readings_controller_test.rb
+++ test/controllers/readings_controller_test.rb
@@ -2,21 +2,22 @@ require "test_helper"
 
 class ReadingsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    File.chmod(0755, Rails.root.join('storage'))
-    Reading.delete_all
+    @id = '36d5658a-6908-479e-887e-a949ec199272'
+    @reading = Reading.create(
+      id: @id,
+      timestamp: "2021-09-29T16:08:15+01:00",
+      count: 2
+    )
   end
-
+  
   test 'create with invalid params' do
-    post readings_url, params: {
-      id: "36d5658a-6908-479e-887e-a949ec199272",
-      readings: []
-    }
+    post readings_url, params: { id: @id, readings: [] }
     assert_response :unprocessable_entity
   end
 
   test 'create with valid params' do
     post readings_url, params: {
-      id: "36d5658a-6908-479e-887e-a949ec199272",
+      id: @id,
       readings: [
         {
           timestamp: "2021-09-29T16:08:15+01:00",
@@ -32,37 +33,12 @@ class ReadingsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'show' do
-    Reading.create(
-      id: "36d5658a-6908-479e-887e-a949ec199272",
-      timestamp: "2021-09-29T16:08:15+01:00",
-      count: 2
-    )
-    get reading_url(id: "36d5658a-6908-479e-887e-a949ec199272")
+    get reading_url(id: @id)
     assert_response :success
   end
 
   test 'show count' do
-    Reading.create(
-      id: "36d5658a-6908-479e-887e-a949ec199272",
-      timestamp: "2021-09-29T16:08:15+01:00",
-      count: 2
-    )
-    get count_reading_url(id: "36d5658a-6908-479e-887e-a949ec199272")
+    get count_reading_url(id: @id)
     assert_response :success
-  end
-
-  test 'file a permission issue' do
-    File.chmod(0000, Rails.root.join('storage'))
-    assert_raise(Errno::EACCES) do
-      Reading.create(
-        id: "36d5658a-6908-479e-887e-a949ec199272",
-        timestamp: "2021-09-29T16:08:15+01:00",
-        count: 2
-      )
-    end
-  end
-
-  def teardown
-    File.chmod(0755, Rails.root.join('storage'))
   end
 end
